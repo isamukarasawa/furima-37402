f5252f88f4f0db1ec320f0a0dbdc6dfb6e30d20e
isamukarasawa <isamukun3@gmail.com>
2022-02-02 12:06:04 +0900

    商品出品機能の修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,7 +8,7 @@ class Item < ApplicationRecord
   validates :delivery_charge_id     , presence: true
   validates :prefecturse_id         , presence: true
   validates :delivery_day_id        , presence: true
-  validates :price                  , presence: true, numericality: {greater_than: 300,less_than: 9999999}, format: { with: /\A[0-9]+\z/ }
+  validates :price                  , presence: true, numericality: {greater_than: 299,less_than: 10000000,only_integer: true}
   validates :image, presence: true
 
   belongs_to :user
